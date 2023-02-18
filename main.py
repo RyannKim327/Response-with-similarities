f76b23962b8082d016879e4d468c172a75dd2ce7
RyannKim327 <rksesgundo123@gmail.com>
2023-02-19 00:19:11 +0800

    02-19-23 00:19 [Created some responses]

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-from utils.check_similar import Similarities
-from utils.response import Reponses
+from utils.check_similar import Similarities, Reponses
 
-a = Similarities("Hello World")
-b = Reponses()
+response = Reponses()
+keys = list(response.getJson().keys())
+response.setName("Nix")
+response.setYourName("RySes")
 
-print(a.compare("Hi world"))
-print(a.compare("Hello World"))
-b.getPercent(a.getAllResponse())
-print(b.getResponse())
+text = input(f"{response.getName()}: Welcome user, my name is {response.getName()}, your vitual chat bot, what do you want to ask for?\nYou: ")
+while True:
+	for i in keys:
+		similar = Similarities(i.lower().replace(response.getName().lower(), response.getKeyName()))
+		similar.compare(text)
+		response.getPercent(similar.getAllResponse())
+		print("Percentage:", response.getResponse()['percentage'])
+		print(response.getKeyName())
+		if response.getResponse()['ok']:
+			print(response.getResponse()['msg'])
+			break
+
+	text = input("You: ")
--- utils/check_similar.py
+++ utils/check_similar.py
@@ -1,6 +1,10 @@
 import re
+import json
+import random
+
 class Similarities:
 	def __init__(self, base: str = ""):
+		'''Just call this first, before the response.'''
 		self.respo = []
 		self.base = base
 	
@@ -20,17 +24,102 @@ class Similarities:
 							if first[i][k].lower() == second[j][l].lower():
 								result += (len(first[i]) / len(second[j]) * 0.1)
 		
+		similar = (result / len(first)) * 100
+
 		self.respo.append({
-			"similar": ((result / len(first)) * 100),
+			"similar": similar,
 			"base": self.base,
 			"input": _input
 		})
 		if isReturn:
 			return {
-				"similar": ((result / len(first)) * 100),
+				"similar": similar,
 				"base": self.base,
 				"input": _input
 			}
 	
 	def getAllResponse(self) -> list[dict]:
 		return self.respo
+
+class Reponses:
+	def __init__(self):
+		self.data = json.loads(open("data/response.json", "r").read())
+		self.bot = "<data>"
+	
+	def getPercent(self, percents: list[dict], isResponse: bool = False) -> dict:
+		key = percents[0]['base']
+		percent = int(percents[0]['similar'])
+		for i in percents:
+			if percent < i['similar']:
+				percent = i['similar']
+				key = i['base']
+		self.response = {
+			"base": key,
+			"similar": percent
+		}
+		if isResponse:
+			return {
+				"base": key,
+				"similar": percent
+			}
+
+	def getKeyName(self) -> str:
+		return self.bot
+
+	def setName(self, name: str):
+		self.name = name
+
+	def getName(self) -> str:
+		return self.name
+
+	def setYourName(self, name: str = ""):
+		self.yourname = name
+
+	def getJson(self) -> dict:
+		return self.data
+
+	def getResponse(self, base: str = "", auto: bool = True) -> dict:
+		base = base.lower()
+		if self.data.get(base) == "" or self.data.get(base) == None:
+			base = self.response['base'].lower()
+			if self.name.lower() in base:
+				base = base.replace(self.name.lower(), self.bot)
+			if self.data.get(base) == None:
+				return {
+					"ok": False,
+					"msg": "I don't know what is this, sorry",
+						"percentage": 0
+				}
+			else:
+				if float(self.response.get("similar")) < 75:
+					return {
+						"ok": False,
+						"msg": "Low Possibility",
+						"percentage": self.response.get("similar")
+					}
+				else:
+					if auto:
+						return {
+							"ok": True,
+							"msg": self.data.get(base)[random.randint(0, len(self.data.get(base)) - 1)].replace("<name>", self.yourname).replace("<myname>", self.name),
+							"percentage": self.response.get("similar")
+						}
+					else:
+						return {
+							"ok": True,
+							"msg": self.data.get(base),
+							"percentage": self.response.get("similar")
+						}
+		else:
+			if auto:
+				return {
+					"ok": True,
+					"msg": self.data.get(base)[random.randint(0, len(self.data.get(base)) - 1)].replace("<name>", self.yourname).replace("<myname>", self.name),
+					"percentage": self.response.get("similar")
+				}
+			else:
+				return {
+					"ok": True,
+					"msg": self.data.get(base),
+					"percentage": self.response.get("similar")
+				}
