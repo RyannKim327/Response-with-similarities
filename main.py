990783b913aba88de6dd7c88b9c0a31836511f09
RyannKim327 <rksesgundo123@gmail.com>
2023-02-18 18:29:15 +0800

    02-18-23 18:29

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from utils.check_similar import Similarities
+from utils.response import Reponses
 
-a = Similarities("Hello World", "Hi World")
-print(a.compare())
+a = Similarities("Hello World")
+b = Reponses()
+
+print(a.compare("Hi world"))
+print(a.compare("Hello World"))
+b.getPercent(a.getAllResponse())
+print(b.getResponse())
--- utils/check_similar.py
+++ utils/check_similar.py
@@ -1,27 +1,36 @@
 import re
 class Similarities:
-	def __init__(self, base: str = "", _input: str = ""):
+	def __init__(self, base: str = ""):
+		self.respo = []
 		self.base = base
-		self.input = _input
 	
-	def compare(self):
+	def compare(self, _input: str = "", isReturn: bool = True) -> dict:
 		first = re.split("\s", self.base)
-		second = re.split("\s", self.input)
+		second = re.split("\s", _input)
 		result = 0
 		
 		for i in range(len(first)):
 			for j in range(i, len(second)):
-				if first[i].lower == second[j].lower:
+				if first[i].lower() == second[j].lower():
 					result += 1
 					break
 				else:
 					for k in range(len(first[i])):
 						for l in range(len(second[j])):
-							if first[i][k].lower == second[j][l].lower:
-								result += 0.1
-
-		return {
-			"similar": (round(result) / len(self.base)) * 100,
+							if first[i][k].lower() == second[j][l].lower():
+								result += (len(first[i]) / len(second[j]) * 0.1)
+		
+		self.respo.append({
+			"similar": ((result / len(first)) * 100),
 			"base": self.base,
-			"input": self.input
-		}
+			"input": _input
+		})
+		if isReturn:
+			return {
+				"similar": ((result / len(first)) * 100),
+				"base": self.base,
+				"input": _input
+			}
+	
+	def getAllResponse(self) -> list[dict]:
+		return self.respo
