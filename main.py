b5f82a7652aabdf23bce84e20532717e6f7e9e2b
RyannKim327 <rksesgundo123@gmail.com>
2023-03-02 09:12:52 +0800

    03-02-23 09:13 [Improve accuracy]

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 from utils.checker import Similarities, Reponses
 
-response = Reponses(percentage=75)
+response = Reponses(percentage=25)
 keys = list(response.getJson().keys())
 response.setName("Nix")
 response.setYourName("RySes")
 
 text = input(f"{response.getName()}: Welcome user, my name is {response.getName()}, your vitual chat bot, what do you want to ask for?\nYou: ")
 while True:
+	data = []
 	for i in keys:
 		similar = Similarities(i.lower().replace(response.getName().lower(), response.getKeyName()))
 		similar.compare(text)
 		response.getPercent(similar.getAllResponse())
-		if response.getResponse()['ok']:
-			print(response.getResponse()['msg'])
-			break
+		data.append(response.getResponse())
+	
+	for i in range(len(data)):
+		for j in range(len(data)):
+			if data[i]['percentage'] < data[j]['percentage']:
+				data[i], data[j] = data[j], data[i]
+	
+	if data[len(data) - 1]['percentage'] == 0:
+		print("No data existed")
 	else:
-		print("I can't see any near match.")
+		print(data[len(data) - 1]['msg'])
 
 	text = input("You: ")
